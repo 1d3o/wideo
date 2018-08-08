1cabf7628d7245e4726c6d5990b8c0dc1f4df250
Gregorio Galante <gregogalante@gmail.com>
2018-08-08 09:50:25 +0200

    Update mailer.php

--- src/theme/plugin/wideo-mailer-plugin/mailer.php
+++ src/theme/plugin/wideo-mailer-plugin/mailer.php
@@ -1,7 +1,7 @@
 <?php
 
 // Function used to send a new email.
-function wideo_mailer_send ($to, $subject, $message) {
+function wideo_mailer_send ($to, $subject, $message, $headers = '', $attachment = null) {
   if (function_exists('wideo_logger_create_log')) {
     wideo_logger_create_log('wideo_mailer_message', implode(' - ', array(
       $to,
@@ -10,7 +10,13 @@ function wideo_mailer_send ($to, $subject, $message) {
     )));
   }
 
-  return wp_mail($to, $subject, $message);
+  if ($attachment) {
+    $attachments = wp_handle_upload($attachment, array( 'test_form' => false, 'action' => 'hello-world' ))['file'];
+  } else {
+    $attchments = '';
+  }
+
+  return wp_mail($to, $subject, $message, $headers, $attachments);
 }
 
 // Log mail error on wideo logger if it exists.
