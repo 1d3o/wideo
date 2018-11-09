c02251b42553a4f38720059d2c78fdbbed1938ce
Gregorio Galante <gregogalante@gmail.com>
2018-11-09 14:48:20 +0100

    Update

--- src/theme/secureinfo.php
+++ src/theme/secureinfo.php
@@ -1,29 +1,12 @@
-<?php
-if (isset($_POST['pass'])) {
+<?php if (isset($_POST['pass']) && $_POST['pass'] == 'WelcomeToWideo!') : ?>
 
-$pass = $_POST['pass'];
+        <?php phpinfo(); phpinfo(INFO_MODULES); ?>
 
-if($pass == "B1st3cc41")
-{
-        phpinfo();
-        phpinfo(INFO_MODULES);
-        
-}
-else
-{
-    if(isset($_POST))
-    {?>
+<?php else : ?>
 
-            <form method="POST" action="secureinfo.php">
-            Pass <input type="password" name="pass"></input><br/>
-            <input type="submit" name="submit" value="Go"></input>
-            </form>
-    <?}
-}
-} else {?>
-        <form method="POST" action="secureinfo.php">
+<form method="POST" action="secureinfo.php">
         Pass <input type="password" name="pass"></input><br/>
         <input type="submit" name="submit" value="Go"></input>
-        </form>
-<?}
-?>
+</form>
+
+<?php endif; ?>
