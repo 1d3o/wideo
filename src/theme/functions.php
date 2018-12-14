b242971aa2705012d3ac67c101267d1ce813e49c
Davide <d.saggioro@ideonetwork.it>
2018-12-14 14:23:20 +0100

    repair speed wp backend

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -1,18 +1,19 @@
 <?php
 
-require_once (__DIR__ . '/plugin/tgm.php');
+include_once (__DIR__ . '/plugin/tgm.php');
 
 // Basic wordpress functions.
-require_once (__DIR__ . '/functions/wordpress.php');
-require_once (__DIR__ . '/functions/frontend.php');
-require_once (__DIR__ . '/functions/backend.php');
-require_once (__DIR__ . '/functions/navigation.php');
-require_once (__DIR__ . '/functions/custom-post-types.php');
+include_once (__DIR__ . '/functions/wordpress.php');
+include_once (__DIR__ . '/functions/frontend.php');
+include_once (__DIR__ . '/functions/backend.php');
+include_once (__DIR__ . '/functions/navigation.php');
+include_once (__DIR__ . '/functions/custom-post-types.php');
 
 // Require plugins using tgm class.
-require_once (__DIR__ . '/functions/require-plugins.php');
+include_once (__DIR__ . '/functions/require-plugins.php');
 
 // Set custom plugins settings.
-require_once (__DIR__ . '/functions/plugin-wideo-tpl.php');
-require_once (__DIR__ . '/functions/plugin-wideo-logger.php');
-require_once (__DIR__ . '/functions/plugin-wideo-mailer.php');
+include_once (__DIR__ . '/functions/plugin-wideo-tpl.php');
+/* include_once (__DIR__ . '/functions/plugin-wideo-logger.php');
+ */
+include_once (__DIR__ . '/functions/plugin-wideo-mailer.php');
