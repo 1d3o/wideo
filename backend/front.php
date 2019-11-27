f11469410bdde19b49cd23500166b1afa365b51b
Davide Saggioro <d.saggioro@ideonetwork.it>
2019-11-27 10:00:56 +0100

    update

--- backend/front.php
+++ backend/front.php
@@ -1,15 +1,37 @@
 <?php
 
+// Remove script and styles
+// ***********************************************************
+
+
+function wideo_deregister_scripts(){
+  wp_deregister_script( 'wp-embed' );
+  wp_deregister_script('jquery');
+
+}
+function wideo_deregister_styles() {
+    wp_dequeue_style( 'wp-block-library' );
+}
+
+add_action( 'wp_enqueue_scripts', 'wideo_deregister_scripts' );
+add_action( 'wp_print_styles', 'wideo_deregister_styles', 100 );
+
+
 // Load scripts on theme.
 // ***********************************************************
 
+
 function wideo_enqueue_scripts() {
   $template_directory = get_template_directory_uri();
-  wp_deregister_script('jquery' ); // NOTE: Remove this if you have problem on front-end without Jquery.
-  wp_enqueue_style('application', $template_directory.'/assets/application.css');
-  wp_enqueue_script('application', $template_directory. '/assets/application.js', '', '1.0.0', true);
-  // NOTE: Add here other bundles for pages
+  // include custom jQuery
+
+    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
+    wp_enqueue_script('application', $template_directory. '/assets/application.js', '', '1.0.0', true);
+    wp_enqueue_style('application', $template_directory.'/assets/application.css');
+
+    // NOTE: Add here other bundles for pages
 }
+
 add_action('wp_enqueue_scripts', 'wideo_enqueue_scripts');
 
 // Clean Wordpress meta tag (active them for a blog).
@@ -25,6 +47,54 @@ remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0); // display relationa
 remove_action('wp_head', 'index_rel_link'); // index link
 remove_action('wp_head', 'parent_post_rel_link', 10, 0); // prev link
 remove_action('wp_head', 'start_post_rel_link', 10, 0); // start link
+remove_action('wp_head', 'rest_output_link_wp_head' );
+remove_action('wp_head', 'wp_oembed_add_discovery_links' );
+remove_action('template_redirect', 'rest_output_link_header', 11 );
+function disable_emojis() {
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+  add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+  add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+add_action( 'init', 'disable_emojis' );
+
+
+/**
+ * Filter function used to remove the tinymce emoji plugin.
+ * 
+ * @param array $plugins 
+ * @return array Difference betwen the two arrays
+ */
+function disable_emojis_tinymce( $plugins ) {
+  if ( is_array( $plugins ) ) {
+  return array_diff( $plugins, array( 'wpemoji' ) );
+  } else {
+  return array();
+  }
+ }
+ 
+ /**
+  * Remove emoji CDN hostname from DNS prefetching hints.
+  *
+  * @param array $urls URLs to print for resource hints.
+  * @param string $relation_type The relation type the URLs are printed for.
+  * @return array Difference betwen the two arrays.
+  */
+ function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+  if ( 'dns-prefetch' == $relation_type ) {
+  /** This filter is documented in wp-includes/formatting.php */
+  $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+ 
+ $urls = array_diff( $urls, array( $emoji_svg_url ) );
+  }
+ 
+ return $urls;
+}
 
 // Update Body classes.
 // ***********************************************************
