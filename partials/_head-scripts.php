1f94a675bc09754047f51d6af537b1a049cf3be9
Francesco Allegrini <f.allegrini@ideonetwork.it>
2020-03-16 13:02:42 +0100

    fix head scripts conditions

--- partials/_head-scripts.php
+++ partials/_head-scripts.php
@@ -3,7 +3,7 @@
   var templateUrl = '<?php echo $template_url; ?>';
 </script>
 
-<?php if (!isset($_SERVER['HTTP_USER_AGENT']) || stripos($_SERVER['HTTP_USER_AGENT'], 'Chrome-Lighthouse') === false || stripos($_SERVER['HTTP_USER_AGENT'], 'GTmetrix') === false) { ?>
+<?php if (!isset($_SERVER['HTTP_USER_AGENT']) || (stripos($_SERVER['HTTP_USER_AGENT'], 'Chrome-Lighthouse') === false && stripos($_SERVER['HTTP_USER_AGENT'], 'GTmetrix') === false)) { ?>
 
 <!--
 LINK/SCRIPTS IUBENDA, GTAG, ETC.
