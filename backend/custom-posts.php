5e36417277ed8aa4acea46dc5a16b32a6a684801
Davide <d.saggioro@1d3o.it>
2020-05-21 17:20:01 +0200

    select icon in function register CPT

--- backend/custom-posts.php
+++ backend/custom-posts.php
@@ -121,7 +121,7 @@ function wideo_register_taxonomy($name, $singular, $multiple, $slug, $postTypes 
 // ***********************************************************
 
 function wideo_register_cpt_and_tax() {
-  // wideo_register_post_type('service', 'Servizio', 'Servizi', 'nome-icona', 'service');
+  // wideo_register_post_type('service', 'Servizio', 'Servizi', 'service', 'nome-icona');
   // wideo_register_taxonomy('category-service', 'Categoria servizio', 'Categorie servizio', 'Categoria servizio', ['service']);
 
   // COMPILE_CODE_HERE: aggiungere registrazione di eventuali custom post type
