99d33813097f5868b42f8d5ec5d5b3b450295b00
Gregorio Galante <me@gregoriogalante.com>
2020-06-28 11:02:44 +0200

    Update ACF.php

--- backend/ACF.php
+++ backend/ACF.php
@@ -15,48 +15,6 @@ include_once( FUNCTION_ACF . 'Repeater.php' );
 
 $prefix = 'site-settings';
 
-// Registro i field disponibili con ACF.
-// ***********************************************************
-
-function wideo_register_field_group() {
-    global $prefix;
-    acf_add_local_field_group([
-        'key' => 'group_settings_' . $prefix,
-        'title' => 'Site Settings',
-        'fields' => [
-            addTab($prefix . __FUNCTION__, 'General Settings'),
-            wysiwyg($prefix . __FUNCTION__, 'Cookie Popup'),
-            addTab($prefix . __FUNCTION__, 'Social Options'),
-            init([
-                'settings' => [
-                    'prefix' => $prefix . __FUNCTION__,
-                    'label' => 'Social Options',
-                ],
-                'elements' => [
-                    url($prefix . __FUNCTION__, 'Social link'),
-                        select($prefix . __FUNCTION__, 'Social icon', [
-                        'facebook' => 'Facebook',
-                        'twitter' => 'Twitter',
-                        'instagram' => 'Instagram',
-                        'youtube' => 'YouTube',
-                    ]),
-                ],
-            ]),
-        ],
-        'location' => [
-            [
-                [
-                    'param' => 'options_page',
-                    'operator' => '==',
-                    'value' => 'site-settings',
-                ],
-            ],
-        ],
-    ]);
-}
-
-add_action('acf/init', 'wideo_register_field_group');
-
 // Imposto la pagina di impostazioni sito.
 // ***********************************************************
 
