14a554308dd225b9c3abc36b8d79965f5973729d
Davide <d.saggioro@1d3o.it>
2021-09-06 09:33:25 +0200

    update component and partials with new get_template_part

--- php-util/components.php
+++ php-util/components.php
@@ -1,32 +1,13 @@
 <?php
 
 /**
- * FUNZIONE PER RECUPERO COMPONENTI DINAMICHE.
- * Mettere le componenti in /partials/components.
- * Richiamare le componenti con <php component('nome-componente', array('config1' => 'value')); ?>
- */
+ 	* FUNZIONE PER RECUPERO COMPONENTI DINAMICHE.
+	* Mettere le componenti in /partials/components.
+	* Richiamare le componenti con <php component('nome-componente', array('config1' => 'value')); ?>
+	* dentro al componente per accedere hai dati c'è la variabile globale $args che all suo interno torna l'array che viene passato al component
+*/
 
-$components_config = array(
-  'hero' => array(
-    'title' => '',
-    'image' => NULL
-  )
-);
 
-$common_config = array(
-  'class' => '',
-  'attributes' => ''
-);
-
-function component ($name = '', $args = array()) {
-  global $globals, $common_config, $components_config;
-  if ($name) {
-    extract($globals);
-    if (isset($components_config[$name])) {
-      extract(array_merge($components_config[$name], $common_config, $args));
-    } else {
-      extract(array_merge($common_config, $args));
-    }
-    include(locate_template("partials/components/_$name.php"));
-  }
+function component ($name = '', $vars = array()) {
+	get_template_part( 'partials/components/_'.$name, '', $vars);
 }
--- php-util/partial.php
+++ php-util/partial.php
@@ -1,15 +1,11 @@
 <?php
-
 /**
  * FUNZIONE PER RICHIAMARE TEMPLATE PARZIALI.
  * Mettere i template in /partials.
  * Richiamare la funzione con <php partial('nome-parziale'); ?>
  */
 
-function partial ($name = '') {
-  if ($name) {
-    global $globals;
-    extract($globals);
-    include(locate_template("partials/_$name.php"));
-  }
+
+function partial($name = '') {	
+	get_template_part( 'partials/_'.$name);
 }
