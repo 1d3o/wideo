d632357996f52e257358b48d68a6a968e9bfbee6
Davide <d.saggioro@1d3o.it>
2023-01-16 16:34:30 +0100

    update

--- partials/_head-scripts.php
+++ partials/_head-scripts.php
@@ -1,6 +1,6 @@
 <script>
-  var siteUrl = '<?php echo $site_url; ?>';
-  var templateUrl = '<?php echo $template_url; ?>';
+var siteUrl = '<?php echo get_site_url( ); ?>';
+var templateUrl = '<?php echo get_template_directory_uri(); ?>';
 </script>
 
 <?php if (!isset($_SERVER['HTTP_USER_AGENT']) || (stripos($_SERVER['HTTP_USER_AGENT'], 'Chrome-Lighthouse') === false && stripos($_SERVER['HTTP_USER_AGENT'], 'GTmetrix') === false)) { ?>
