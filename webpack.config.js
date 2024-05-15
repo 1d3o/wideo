f5242b001ecb689c74ecf9868640d47647ccb6ba
Davide <d.saggioro@1d3o.it>
2024-05-15 12:02:41 +0200

    update webpack to versione 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,152 +1,162 @@
-const path = require('path')
-const fs = require('fs-extra')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const autoprefixer = require('autoprefixer')
+const path = require("path");
+const fs = require("fs-extra");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require("autoprefixer");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, args) => {
+  const isDev = args.mode === "development";
 
   // Generate Wideo config
-  if (!fs.existsSync('./.wideo.json')) {
-    const wideo = {
+  const wideoConfigPath = "./.wideo.json";
+  if (!fs.existsSync(wideoConfigPath)) {
+    const wideoConfig = {
       ignoreFiles: [
-        '.wideo.json', 'webpack.config.js', 'README.md', 'package.json', 'package-lock.json', 'yarn.lock', '.gitignore',
-        'src', 'build', 'node_modules', 'bower_components', '.git', 'docs', '.editorconfig',
-        'docker-compose.yml', '.dockerignore', 'docker', 'cache'
-      ]
-    }
-    fs.writeFileSync('./.wideo.json', JSON.stringify(wideo, null, 2), 'utf8')
+        ".wideo.json",
+        "webpack.config.js",
+        "README.md",
+        "package.json",
+        "package-lock.json",
+        "yarn.lock",
+        ".gitignore",
+        "src",
+        "build",
+        "node_modules",
+        "bower_components",
+        ".git",
+        "docs",
+        ".editorconfig",
+        "docker-compose.yml",
+        ".dockerignore",
+        "docker",
+        "cache",
+      ],
+    };
+    fs.writeFileSync(
+      wideoConfigPath,
+      JSON.stringify(wideoConfig, null, 2),
+      "utf8"
+    );
   }
 
   // Load config, variables and entries
-  const wideoConfig = require('./.wideo.json')
-  const isDev = args.mode == 'development'
-  const entry = {}
-  const plugins = []
-  fs.readdirSync('./src/packs').forEach((fileName) => {
-    const key = fileName.split('.')[0]
-    if (!entry[key]) entry[key] = []
-    entry[key].push(`./src/packs/${fileName}`)
-  })
+  const wideoConfig = require(wideoConfigPath);
+  const entry = {};
+  const plugins = [];
+
+  fs.readdirSync("./src/packs").forEach((fileName) => {
+    const key = path.parse(fileName).name;
+    entry[key] = entry[key] || [];
+    entry[key].push(`./src/packs/${fileName}`);
+  });
 
   // Define custom build plugin
-  // - create/detroy build path plugin
   plugins.push({
     apply: (compiler) => {
-      compiler.hooks.beforeCompile.tap('WideoCleanTheme', (params) => {
-        fs.removeSync('./build')
-      })
-      compiler.hooks.afterCompile.tap('WideoBuildTheme', (params) => {
+      compiler.hooks.beforeCompile.tap("WideoCleanTheme", () => {
+        fs.removeSync("./build");
+      });
+      compiler.hooks.afterCompile.tap("WideoBuildTheme", () => {
         if (!isDev) {
-          try { fs.mkdirSync('./build') } catch(e) {}
-          fs.readdirSync('./').forEach((fileName) => {
+          fs.mkdirSync("./build", { recursive: true });
+          fs.readdirSync("./").forEach((fileName) => {
             if (!wideoConfig.ignoreFiles.includes(fileName)) {
-              fs.copySync(`./${fileName}`, `./build/${fileName}`)
+              fs.copySync(`./${fileName}`, `./build/${fileName}`);
             }
-          })
+          });
         }
-      })
-    }
-  })
-  // - copy webpack plugin for fonts and images
+      });
+    },
+  });
+
+  // Copy webpack plugin for fonts and images
   plugins.push(
-    new CopyWebpackPlugin([
-      { from: "src/images", to: "images", ignore: ['*.DS_Store'], },
-      { from: "src/fonts", to: "fonts", ignore: ['*.DS_Store'], }
-    ], {})
-  )
-  // - mini css extract plugin
-  plugins.push(
-    new MiniCssExtractPlugin()
-  )
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "src/images",
+          to: "images",
+          globOptions: { ignore: ["*.DS_Store"] },
+        },
+        {
+          from: "src/fonts",
+          to: "fonts",
+          globOptions: { ignore: ["*.DS_Store"] },
+        },
+      ],
+    })
+  );
 
+  // Mini CSS Extract Plugin
+  plugins.push(new MiniCssExtractPlugin({ filename: "[name].css" }));
 
-  // Define ruleScss
-  const ruleScss = {
-    test: /\.s?css$/,
-    use: [
-      MiniCssExtractPlugin.loader,
-      {
-        loader: 'css-loader',
+  // Rules definitions
+  const rules = [
+    {
+      test: /\.s?css$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        { loader: "css-loader", options: { sourceMap: isDev, url: false } },
+        {
+          loader: "postcss-loader",
+          options: {
+            postcssOptions: { plugins: [autoprefixer()] },
+            sourceMap: isDev,
+          },
+        },
+        { loader: "sass-loader", options: { sourceMap: isDev } },
+      ],
+    },
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: "babel-loader",
         options: {
-          sourceMap: isDev,
-          url: false
+          presets: [["@babel/preset-env", { modules: false }]],
+          plugins: [["@babel/plugin-proposal-class-properties"]],
         },
       },
-      {
-        ident: 'postcss',
-        loader: 'postcss-loader',
-        options: {
-          plugins: () => [
-            autoprefixer(),
-          ],
-        },
+    },
+    {
+      test: /\.(woff(2)?|ttf|eot|svg)$/,
+      type: "asset/resource",
+      generator: {
+        filename: "fonts/[name][ext][query]",
       },
-      {
-        loader: 'sass-loader',
-        options: {
-          sourceMap: isDev,
-        },
+      exclude: /src\/images/,
+    },
+    {
+      test: /\.(svg)$/,
+      type: "asset/resource",
+      generator: {
+        filename: "images/[name][ext][query]",
       },
-    ]
-  }
+      exclude: /src\/fonts/,
+    },
+  ];
 
-  // Define ruleJs
-  const ruleJs = {
-    test: /\.js$/,
-    exclude: /(node_modules|bower_components)/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: [["@babel/preset-env", { modules: false }]],
-        plugins: [['@babel/plugin-proposal-class-properties']]
-      }
-    }
-  }
-  
-  // Default ruleFont
-  const ruleFont = {
-    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-    exclude: /images/,  /* dont want svg images from image folder to be included */
-
-    use: [{
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]',
-        outputPath: 'fonts/'
-      }
-    }]
-  }
-
-  // Default ruleSvg
-  const ruleSvg = {
-    test: /\.(svg)$/,
-    exclude: /fonts/, /* dont want svg fonts from fonts folder to be included */
-    use: [{
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]',
-        outputPath: 'images/',
-      }
-    }]
-  }
-
-  // Final return
   return {
-    devtool: isDev ? 'cheap-module-eval-source-map' : false,
-
+    target: "web", // Added target
+    devtool: isDev ? "eval-cheap-module-source-map" : false,
     mode: args.mode,
-    watch: isDev,
     entry: entry,
-    
     output: {
-      filename: '[name].js',
-      path: path.resolve(__dirname, 'assets')
+      filename: "[name].js",
+      path: path.resolve(__dirname, "assets"),
+      clean: true, // Automatically clean the output directory
     },
-    module: {
-      rules: [ruleFont, ruleScss, ruleJs, ruleSvg]
+    module: { rules: rules },
+    plugins: plugins,
+    watch: isDev, // Ensure this is only set in dev mode
+    optimization: {
+      minimize: !isDev,
+      minimizer: [
+        new TerserWebpackPlugin({
+          terserOptions: { compress: { drop_console: !isDev } },
+        }),
+      ],
     },
-    plugins: plugins
-  }
-
-}
+  };
+};
